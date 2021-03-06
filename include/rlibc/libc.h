1d21fac1b2146832aba0f3529b2cef236fb73f64
Berrysoft <Strawberry_Str@hotmail.com>
2021-03-06 21:43:31 +0800

    wctype.h

--- include/rlibc/libc.h
+++ include/rlibc/libc.h
@@ -18,30 +18,13 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <utime.h>
+#include <wchar.h>
+#include <wctype.h>
 
 typedef unsigned long clock_t;
 typedef long time_t;
-typedef uint32_t wchar_t;
-typedef uint32_t wint_t;
 
 #define NULL (0)
-
-/* Types */
-
-int iswalnum(wint_t);
-int iswalpha(wint_t);
-int iswblank(wint_t);
-int iswcntrl(wint_t);
-int iswdigit(wint_t);
-int iswgraph(wint_t);
-int iswlower(wint_t);
-int iswprint(wint_t);
-int iswpunct(wint_t);
-int iswspace(wint_t);
-int iswupper(wint_t);
-int iswxdigit(wint_t);
-wint_t towlower(wint_t);
-wint_t towupper(wint_t);
 
 /* Memory Management */
 int brk(void*);
---
+++ include/rlibc/wchar.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <stdint.h>
+typedef uint32_t wchar_t;
+typedef uint32_t wint_t;
